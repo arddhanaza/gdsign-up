edf19c1c76e9cf2626c77f690fd45da96fcb25d2
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2016-09-10 16:36:48 +0700

    (3)
    validasi radio button dan checkbox

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1003/graphicdesignup/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1003/graphicdesignup/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox cbPS, cbAI, cbCD;
     EditText etNama, etEmail;
     Button bSU;
     TextView tvHasil;
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cbAI = (CheckBox) findViewById(R.id.checkBoxAI);
+        cbPS = (CheckBox) findViewById(R.id.checkBoxPS);
+        cbCD = (CheckBox) findViewById(R.id.checkBoxCorel);
         etNama = (EditText) findViewById(R.id.editTextNama);
         etEmail = (EditText) findViewById(R.id.editTextEmail);
         bSU = (Button) findViewById(R.id.buttonSU);
@@ -32,12 +38,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     private void doClick() {
+        String nama = etNama.getText().toString();
+        String email = etEmail.getText().toString();
+        String hasil = null;
+        String hasilcb = "\nSoftware yang Anda Kuasai: \n";
+        int startlen = hasilcb.length();
+
         if (isValid()) {
-            String nama = etNama.getText().toString();
-            String email = etEmail.getText().toString();
-            tvHasil.setText("Nama Anda: " + nama + "\nEmail Anda: " + email);
+
+            if (rgKelas.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton)
+                        findViewById(rgKelas.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            if (hasil == null) {
+                tvHasil.setText("Jenis Kelamin Belum Diisi");
+            } else {
+                if (cbAI.isChecked()) hasilcb += cbAI.getText() + "\n";
+                if (cbCD.isChecked()) hasilcb += cbCD.getText() + "\n";
+                if (cbPS.isChecked()) hasilcb += cbPS.getText() + "\n";
+                if (hasilcb.length() == startlen) hasilcb += "Tidak ada Pilihan";
+
+                tvHasil.setText("Nama Anda: " + nama + "\nEmail Anda: " + email + "\nJenis Kelamin: " + hasil + hasilcb);
+            }
+
+
         }
+
 
     }
 
@@ -66,6 +95,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return valid;
     }
-
 }
 
